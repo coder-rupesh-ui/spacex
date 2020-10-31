840ec1ac28e352f08b2f8dfc1bdb5186d2740386
coder-rupesh-ui <73532919+coder-rupesh-ui@users.noreply.github.com>
2020-10-31 13:31:49 +0530

    Update app.service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppService {
+    private rootUrl = 'https://api.spaceXdata.com/v3/launches?limit=100';
+    constructor(private http: HttpClient){}
+
+    getData(param) {
+        return this.http.get(this.rootUrl, {
+            params: param
+        });
+    }
 }
