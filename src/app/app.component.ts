d76c47c565c55e37c237847cf3e88dd654f1de77
coder-rupesh-ui <73532919+coder-rupesh-ui@users.noreply.github.com>
2020-10-31 10:29:12 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spacex';
+  startYear=2001;
+  endYear=2020;
+  years = [];
+  successfulLaunch: boolean;
+  successfulLanding: boolean;
+
+  ngOnInit() {
+    for(let i=this.startYear;i<=this.endYear;i++) {
+      this.years.push({
+        label: i,
+        selected: false
+      });  
+    }
+  }
+
+  selectYear(year) {
+    this.clearYearSelection();
+    year.selected = true;
+  }
+
+  clearYearSelection() {
+    this.years.forEach((year) => {
+      year.selected = false;
+    })
+  }
+
+  selectLaunch(flag) {
+    this.successfulLaunch = flag;
+  }
+
+  selectLanding(flag) {
+    this.successfulLanding = flag;
+  }
 }
