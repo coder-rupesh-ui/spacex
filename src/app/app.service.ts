5c15e8fa63b6581a02a45b61c4d3243604354b95
coder-rupesh-ui <73532919+coder-rupesh-ui@users.noreply.github.com>
2020-10-31 17:11:09 +0530

    Update app.service.ts

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppService {
     private rootUrl = 'https://api.spaceXdata.com/v3/launches?limit=100';
+    private filter = new BehaviorSubject<any>({});
+    public filter$ = this.filter.asObservable();
     constructor(private http: HttpClient){}
 
     getData(param) {
         return this.http.get(this.rootUrl, {
             params: param
         });
+    }
+
+    public setParams(param) {
+        this.filter.next(param);
     }
 }
